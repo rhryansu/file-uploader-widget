58bcc1f6e012b6b2c0de06b6d6816b1f9673c0b7
Ryan-Win <rh.ryan.su@gmail.com>
2022-05-22 00:20:50 +1000

    v1.0 init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action='http://localhost:5000/uploader' method='POST' encType='multipart/form-data'>
+        <input type='file' name='file'/>
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   );
 }
