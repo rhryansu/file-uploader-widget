7c5c1fc150d72ce801d56e6851896a01b3ea9220
Ryan-Win <rh.ryan.su@gmail.com>
2022-05-22 01:03:42 +1000

    v2.0 functionality and style added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <form action='http://localhost:5000/uploader' method='POST' encType='multipart/form-data'>
-        <input type='file' name='file'/>
-        <button type='submit'>Submit</button>
+      <form action='http://localhost:5000/uploader' method='POST' source="custom" encType='multipart/form-data'>
+        <input type='file' name='file' class="u-border-1 u-border-grey-30 u-input u-input-rectangle" id='fileuploader'/>
+        <button type='submit' className="u-black u-border-none u-btn u-btn-submit u-button-style u-btn-1" id='submitbutton'>Submit</button>
       </form>
     </div>
   );
